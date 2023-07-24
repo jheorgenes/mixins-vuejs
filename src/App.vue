dac82f79f86959de2a4e1e0c4ab5d1217004692b
Jheorgenes Warlley <jheorgenes@gmail.com>
2023-07-24 17:52:50 -0300

    - Aprendendo a trabalhar com Mixins

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
-    <lista-filmes />
-    <lista-pessoas />
-    <lista-videogames />
+  <div style="display: flex; flex-direction: row">
+    <div style="flex-basis: 33%">
+      <lista-filmes />
+    </div>
+    <div style="flex-basis: 33%">
+      <lista-pessoas />
+    </div>
+    <div style="flex-basis: 33%">
+      <lista-videogames />
+    </div>
   </div>
 </template>
 
--- src/components/ListaFilmes.vue
+++ src/components/ListaFilmes.vue
@@ -1,10 +1,22 @@
 <template>
-  <h3>Lista Filmes</h3>
+  <div>
+    <h3>Lista Filmes</h3>
+    <input type="text" placeholder="Nome do Filme" v-model="item">
+    <button type="button" @click="adicionarItem">Adicionar</button>
+    <br>
+    <ul>
+      <li v-for="(item, index) in itens" :key="index">{{ item }}</li>
+    </ul>
+    <button type="button" @click="removerItem">Remover</button>
+  </div>
 </template>
 
 <script>
+import ListaMixin from '@/mixins/ListaMixin'
+import RemoverItemListaMixin from '@/mixins/RemoverItemListaMixin'
 
 export default {
-  name: 'ListaFilmes'
+  name: 'ListaVideogames',
+  mixins: [ListaMixin, RemoverItemListaMixin]
 }
 </script>
