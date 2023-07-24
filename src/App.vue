628196bf93baf5cd2bbbde4c403bfdbd76bce30c
Jheorgenes Warlley <jheorgenes@gmail.com>
2023-07-24 16:24:11 -0300

    - Início do projeto de mixins

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <lista-filmes />
+    <lista-pessoas />
+    <lista-videogames />
+  </div>
+</template>
+
+<script>
+import ListaFilmes from '@/components/ListaFilmes.vue'
+import ListaPessoas from '@/components/ListaPessoas.vue'
+import ListaVideogames from '@/components/ListaVideogames.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ListaFilmes,
+    ListaPessoas,
+    ListaVideogames,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ListaFilmes.vue
@@ -0,0 +1,10 @@
+<template>
+  <h3>Lista Filmes</h3>
+</template>
+
+<script>
+
+export default {
+  name: 'ListaFilmes'
+}
+</script>
